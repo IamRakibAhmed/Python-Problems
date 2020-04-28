a7236fea199d820c6025ef2e98e1af31527e9482
Rakib Ahmed <rakibofficial@gmail.com>
2020-04-29 04:19:36 +0600

    Create xmlParse1.py

---
+++ xmlParse1.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+
+
+
+data = '''
+
+<person>
+
+<name> Rakib </name>
+
+<phone type = 'intl'> +8801300000000 </phone>
+
+<email hide = 'yes'/>
+
+</person>'''
+
+
+
+tree = ET.fromstring(data)
+
+print('Name:', tree.find('name').text)
+
+print('Phone type:', tree.find('phone').get('type'))
+
+print('Phone number:', tree.find('phone').text)
+
+print('Email:', tree.find('email').get('hide'))
